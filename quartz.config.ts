78c1c36082b318c5f293c1edbbc4346961606cde
stefanodalessio <8409032+stefanodalessio@users.noreply.github.com>
2024-11-19 11:29:37 +0100

    Quartz sync: Nov 19, 2024, 11:29 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ploc",
+    pageTitle: "ploc' [ˈplɔʧ] Stefano D'Alessio",
     enableSPA: true,
     enablePopovers: true,
     analytics: {

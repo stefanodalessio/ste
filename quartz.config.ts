2432835cac8aa6e230d6a09cfdfa1daaf47c4de2
stefanodalessio <8409032+stefanodalessio@users.noreply.github.com>
2025-01-30 14:52:08 +0100

    Quartz sync: Jan 30, 2025, 2:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -45,7 +45,7 @@ const config: QuartzConfig = {
           darkgray: "#d4d4d4",
           dark: "#ebebec",
           secondary: "#af34f6",
-          tertiary: "#ff9527",
+          tertiary: "#fe45b3",
           highlight: "rgba(255, 43, 244, 0.36)",
           textHighlight: "#b3aa0288",
         },

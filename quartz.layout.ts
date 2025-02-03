7342cb12d8bcdc89f82b7ce8a1c292f30667f5c2
stefanodalessio <8409032+stefanodalessio@users.noreply.github.com>
2025-02-03 22:12:44 +0100

    Quartz sync: Feb 3, 2025, 10:12 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,7 +27,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     //Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      // sd.changed {}
+      filterFn: (node) => {
+        // set containing names of everything you want to filter out
+        const omit = new Set(["authoring content", "tags", "hosting", "unlisteddd", "FH24WS stuff to install", "Stage-Based interaction: stuff to install", "Łódź workshop to install"])
+        return !omit.has(node.name.toLowerCase())
+      },
+    }
+
+
+    )),
   ],
   right: [
     Component.Graph(),
@@ -44,7 +54,18 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     //Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+
+      // sd.changed {}
+      filterFn: (node) => {
+        // set containing names of everything you want to filter out
+        const omit = new Set(["authoring content", "tags", "hosting"])
+        return !omit.has(node.name.toLowerCase())
+      },
+    }
+
+
+    )),
   ],
   right: [],
 }
